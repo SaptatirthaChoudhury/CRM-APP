8a5e5ec806d8eb2db373434b90e1d2c35e030ce2
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-04-16 20:30:01 +0530

    fixed email validation bug

--- middlewares/verifySignup.js
+++ middlewares/verifySignup.js
@@ -87,8 +87,8 @@ validateSignupRequestBody = async (req, res, next) => {
 
     }
 
-    async function isValidEmail(email) {
-        return await emailValidator.validate(email)
+    const isValidEmail = (email) => {
+        return String(email).toLocaleLowerCase().match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     }
 
 
