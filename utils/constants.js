c89e8dcfcdb139fb26f8d22a976bc81e43293bbe
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-04-16 14:18:09 +0530

    added constants in utils

---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    userTypes: {
+        customer: "CUSTOMER",
+        admin: "ADMIN",
+        engineer: "ENGINEER"
+    },
+
+    userStatus: {
+        pending: "PENDING",
+        approved : "APPROVED",
+        rejected : "REJECTED"
+
+    }
+}
