189d13b0e5d2459c47b6f8437cbb6f40f58ed4af
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-04-26 22:01:01 +0530

    more route added

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,4 +4,10 @@ const { authJwt } = require("../middlewares");
 
 module.exports = (app) => {
     app.get("/crm/api/v1/users", [authJwt.verifyToken, authJwt.isAdmin], userController.findAll);
-} 
+
+    app.get("/crm/api/v1/users/:id", [authJwt.verifyToken, authJwt.isValidUserIdReqParam, authJwt.isAdminOrOwner], userController.findByUserId);
+
+    app.put("/crm/api/v1/users/:id", [authJwt.verifyToken, authJwt.isValidUserIdReqParam, authJwt.isAdminOrOwner], userController.update)
+
+
+}   
