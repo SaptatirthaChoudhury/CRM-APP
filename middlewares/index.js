9a3c281756db94a6ae2668b901d187bb16c22cf4
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-05-12 12:17:15 +0530

    listed middleware-> [verifyUser] in index.js file

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,12 @@
 const verifySignUp = require("./verifySignup");
-const authJwt = require("./auth.jwt") 
+const authJwt = require("./auth.jwt");
+const verifyUser = require("./checkForValidUser");
 
 /**
  * I can add more middleware here as the project grows
  */
 module.exports = {
     verifySignUp,
-    authJwt
+    authJwt,
+    verifyUser
 }
